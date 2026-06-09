26b75eebf571a7d904fbf44af5d3998920ed4132
YukiJanse <yuki.janse@gmail.com>
2026-06-10 00:14:14 +0200

    Modify endpoint paths

--- src/api/products.js
+++ src/api/products.js
@@ -1,8 +1,8 @@
 import { request } from './client'
 
 export const productsApi = {
-    getAll: () => request('products'),
-    getById: (id) => request(`products/${id}`),
+    getAll: () => request('products/public'),
+    getById: (id) => request(`products/public/${id}`),
     checkInventory: (id) => request(`products/inventory/${id}`),
     getCategories: () => request('products/categories'),
     getByCategory: (cat) => request(`products/category/${encodeURIComponent(cat)}`),
